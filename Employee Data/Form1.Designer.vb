fdb7ec61846520bf9c7f876719d34b5c8dd243bc
r-shah1 <89700811+r-shah1@users.noreply.github.com>
2022-12-23 16:17:09 -0500

    Add files via upload

---
+++ Employee Data/Form1.Designer.vb
@@ -0,0 +1,281 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtMiddleName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtEmployeeNum = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtExtension = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.cboxDepartment = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(31, 22)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(313, 336)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Employee Data"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(83, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "First Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(69, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Middle Name:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(83, 137)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Last Name:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(83, 236)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 17)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Telephone:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(77, 203)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Department:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(35, 170)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(128, 17)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Employee Number:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(117, 302)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(46, 17)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Email:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(90, 269)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(73, 17)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "Extension:"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(174, 68)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(154, 23)
+        Me.txtFirstName.TabIndex = 9
+        '
+        'txtMiddleName
+        '
+        Me.txtMiddleName.Location = New System.Drawing.Point(174, 101)
+        Me.txtMiddleName.Name = "txtMiddleName"
+        Me.txtMiddleName.Size = New System.Drawing.Size(154, 23)
+        Me.txtMiddleName.TabIndex = 10
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(174, 134)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(154, 23)
+        Me.txtLastName.TabIndex = 11
+        '
+        'txtEmployeeNum
+        '
+        Me.txtEmployeeNum.Location = New System.Drawing.Point(174, 167)
+        Me.txtEmployeeNum.Name = "txtEmployeeNum"
+        Me.txtEmployeeNum.Size = New System.Drawing.Size(154, 23)
+        Me.txtEmployeeNum.TabIndex = 12
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(174, 299)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(154, 23)
+        Me.txtEmail.TabIndex = 16
+        '
+        'txtExtension
+        '
+        Me.txtExtension.Location = New System.Drawing.Point(174, 266)
+        Me.txtExtension.Name = "txtExtension"
+        Me.txtExtension.Size = New System.Drawing.Size(154, 23)
+        Me.txtExtension.TabIndex = 15
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(174, 233)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(154, 23)
+        Me.txtPhone.TabIndex = 14
+        '
+        'cboxDepartment
+        '
+        Me.cboxDepartment.FormattingEnabled = True
+        Me.cboxDepartment.Items.AddRange(New Object() {"Accounting", "Administration", "Marketing", "MIS", "Sales"})
+        Me.cboxDepartment.Location = New System.Drawing.Point(174, 200)
+        Me.cboxDepartment.Name = "cboxDepartment"
+        Me.cboxDepartment.Size = New System.Drawing.Size(154, 24)
+        Me.cboxDepartment.TabIndex = 17
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(31, 408)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(99, 42)
+        Me.btnSave.TabIndex = 18
+        Me.btnSave.Text = "&Save Record"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(136, 408)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(98, 42)
+        Me.btnClear.TabIndex = 19
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(240, 408)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(104, 42)
+        Me.btnExit.TabIndex = 20
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblStatus.Location = New System.Drawing.Point(52, 368)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(276, 28)
+        Me.lblStatus.TabIndex = 21
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(381, 471)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cboxDepartment)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtExtension)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtEmployeeNum)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtMiddleName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Employee Data"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtMiddleName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtEmployeeNum As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtExtension As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents cboxDepartment As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+
+End Class
---
+++ Employee Data/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Dim strFilename As String
+
+Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    'Get the desired Filename from the user
+    strFilename = InputBox("Enter the Filename")
+End Sub
+
+Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+    'Constant for number of inputs
+    Const intINPUT_SET As Integer = 1
+
+    'Local Variables
+    Dim strFirstName As String
+    Dim strMiddleName As String
+    Dim strLastName As String
+    Dim strEmployeeNum As String
+    Dim strDepartment As String
+    Dim strPhone As String
+    Dim strExtension As String
+    Dim strEmail As String
+    Dim friendFile As StreamWriter
+
+    Try
+        'Open the file and Append text
+        friendFile = File.AppendText(strFilename)
+
+        For intCount = 1 To intINPUT_SET
+            'Get Inputs from User
+            strFirstName = txtFirstName.Text
+            strMiddleName = txtMiddleName.Text
+            strLastName = txtLastName.Text
+            strEmployeeNum = txtEmployeeNum.Text
+            strDepartment = cboxDepartment.SelectedItem.ToString()
+            strPhone = txtPhone.Text
+            strExtension = txtExtension.Text
+            strEmail = txtEmail.Text
+
+            'Write data to the file
+            friendFile.WriteLine(strFirstName)
+            friendFile.WriteLine(strMiddleName)
+            friendFile.WriteLine(strLastName)
+            friendFile.WriteLine(strEmployeeNum)
+            friendFile.WriteLine(strDepartment)
+            friendFile.WriteLine(strPhone)
+            friendFile.WriteLine(strExtension)
+            friendFile.WriteLine(strEmail)
+        Next
+
+        'Auto-Close File to save data
+        friendFile.Close()
+
+        'Update Status Bar
+        lblStatus.Text = "Your File " & strFilename & " has been created!"
+
+    Catch ex As Exception
+        'Error Message
+        MessageBox.Show("Error: This File Cannot Be Created.")
+    End Try
+
+End Sub
+
+Friend WithEvents lblStatus As Label
+
+Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+    'Clear All Fields
+    txtFirstName.Text = ""
+    txtMiddleName.Text = ""
+    txtLastName.Text = ""
+    txtEmployeeNum.Text = ""
+    cboxDepartment.SelectedIndex = -1
+    txtPhone.Text = ""
+    txtExtension.Text = ""
+    txtEmail.Text = ""
+    lblStatus.Text = ""
+End Sub
+
+Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Me.Close()
+End Sub
+
+Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+    'Shortcut exit key set to Alt + X
+    If e.KeyCode = Keys.X AndAlso e.Modifiers = Keys.Alt Then
+        e.Handled = True
+        btnExit_Click(sender, e) 'or cmdExit.PerformClick()
+    End If
+
+    'Shortcut clear key set to Alt + C
+    If e.KeyCode = Keys.C AndAlso e.Modifiers = Keys.Alt Then
+        e.Handled = True
+        btnClear_Click(sender, e)
+    End If
+
+    'Shortcut save key set to Alt + S
+    If e.KeyCode = Keys.S AndAlso e.Modifiers = Keys.Alt Then
+        e.Handled = True
+        btnSave_Click(sender, e)
+    End If
+End Sub
+
+
